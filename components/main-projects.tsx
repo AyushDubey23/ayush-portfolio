8b08b03baae98047f9289b9784de548d070a15b5
AyushDubey23 <ayushdubey3740@gmail.com>
2025-06-16 00:17:27 +0530

    Update main-projects.tsx

--- components/main-projects.tsx
+++ components/main-projects.tsx
@@ -48,7 +48,7 @@ export default function MainProjects({ darkMode }: MainProjectsProps) {
       tech: ["TensorFlow.js", "JavaScript", "HTML5", "CSS3", "Local Storage"],
       liveUrl: "https://ayushdubey23.github.io/Machine-Learning-Project-Vision-AI-/",
       githubUrl: "#",
-      gif: "/assets/proj4.gif",
+      gif: "/assets/proj4.gif"
     },
   ];
 
